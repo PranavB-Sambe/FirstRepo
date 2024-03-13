e3cb1665ba490583a0c162424b9e98486099f15a
Pranav Bhoola <pranav.bhoola@sambeconsulting.com>
2024-03-13 19:51:22 +0200

    Added Stored Procedure

--- UpdateScripts/Scripts.sql
+++ UpdateScripts/Scripts.sql
@@ -22,6 +22,32 @@ VALUES
     (1, 'John', 'Doe', 'Math', 'A'),
     (2, 'Jane', 'Smith', 'English', 'B'),
     (3, 'Bob', 'Johnson', 'Science', 'C'),
-    (4, 'Alice', 'Williams', 'History', 'A'),
-    (5, 'Pranav', 'Bhoola', 'English', 'A');
+    (4, 'Alice', 'Williams', 'History', 'A');
+    
 GO
+
+-- Use the StudentDatabase
+USE StudentDatabase;
+GO
+
+-- Create the InsertStudent stored procedure
+CREATE PROCEDURE InsertStudent
+    @StudentID INT,
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @Subject NVARCHAR(50),
+    @Grade VARCHAR(5)
+AS
+BEGIN
+    INSERT INTO StudentInformation (StudentID, FirstName, LastName, Subject, Grade)
+    VALUES (@StudentID, @FirstName, @LastName, @Subject, @Grade);
+END;
+GO
+
+-- Insert data using the InsertStudent stored procedure
+EXEC InsertStudent
+    @StudentID = 5,
+    @FirstName = 'Pranav',
+    @LastName = 'Bhoola',
+    @Subject = 'Physics',
+    @Grade = 'A';
