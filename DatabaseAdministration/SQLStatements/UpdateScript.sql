d3e6767d7c2b086809b1207e496d742ab39cb838
Pranav Bhoola <pranav.bhoola@sambeconsulting.com>
2024-03-18 11:55:17 +0200

    added second table to script

--- DatabaseAdministration/SQLStatements/UpdateScript.sql
+++ DatabaseAdministration/SQLStatements/UpdateScript.sql
@@ -23,8 +23,19 @@ VALUES
     
 GO
 
--- Use the StudentDatabase
-USE StudentDatabase;
+-- Create the SubjectInformation table
+CREATE TABLE SubjectInformation (
+    SubjectID INT PRIMARY KEY,
+    SubjectName NVARCHAR(50),
+    Teacher NVARCHAR(50)
+);
+GO
+
+-- Insert sample data into the SubjectInformation table
+INSERT INTO SubjectInformation (SubjectID, SubjectName, Teacher)
+VALUES
+    (1, 'Math', 'Mr. Johnson');
+    
 GO
 
 -- Create the InsertStudent stored procedure
@@ -41,10 +52,28 @@ BEGIN
 END;
 GO
 
+-- Create the InsertSubject stored procedure
+CREATE PROCEDURE InsertSubject
+    @SubjectID INT,
+    @SubjectName NVARCHAR(50),
+    @Teacher NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO SubjectInformation (SubjectID, SubjectName, Teacher)
+    VALUES (@SubjectID, @SubjectName, @Teacher);
+END;
+GO
+
+-- Insert data using the InsertSubject stored procedure
+EXEC InsertSubject
+    @SubjectID = 2,
+    @SubjectName = 'Physics',
+    @Teacher = 'Ms. Parker'; 
+
 -- Insert data using the InsertStudent stored procedure
 EXEC InsertStudent
     @StudentID = 2,
     @FirstName = 'Pranav',
     @LastName = 'Bhoola',
     @Subject = 'Physics',
-    @Grade = 'A';
+    @Grade = 'A'; 
