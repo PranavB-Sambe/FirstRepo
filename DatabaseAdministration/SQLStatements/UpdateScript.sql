0b8490cb55ec6d2fe6c2da4a57454bc9d8830c2f
Pranav Bhoola <pranav.bhoola@sambeconsulting.com>
2024-03-18 11:27:12 +0200

    added scripts

---
+++ DatabaseAdministration/SQLStatements/UpdateScript.sql
@@ -0,0 +1,50 @@
+-- Create the StudentDatabase
+CREATE DATABASE StudentDatabase;
+GO
+
+-- Use the StudentDatabase
+USE StudentDatabase;
+GO
+
+-- Create the StudentInformation table
+CREATE TABLE StudentInformation (
+    StudentID INT PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Subject NVARCHAR(50),
+    Grade VARCHAR(5)
+);
+GO
+
+-- Insert sample data into the StudentInformation table
+INSERT INTO StudentInformation (StudentID, FirstName, LastName, Subject, Grade)
+VALUES
+    (1, 'John', 'Smith', 'Math', 'A');
+    
+GO
+
+-- Use the StudentDatabase
+USE StudentDatabase;
+GO
+
+-- Create the InsertStudent stored procedure
+CREATE PROCEDURE InsertStudent
+    @StudentID INT,
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @Subject NVARCHAR(50),
+    @Grade VARCHAR(5)
+AS
+BEGIN
+    INSERT INTO StudentInformation (StudentID, FirstName, LastName, Subject, Grade)
+    VALUES (@StudentID, @FirstName, @LastName, @Subject, @Grade);
+END;
+GO
+
+-- Insert data using the InsertStudent stored procedure
+EXEC InsertStudent
+    @StudentID = 2,
+    @FirstName = 'Pranav',
+    @LastName = 'Bhoola',
+    @Subject = 'Physics',
+    @Grade = 'A';
